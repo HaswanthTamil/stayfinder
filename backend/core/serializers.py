9eef59dc8c2c243ae421411b57d368ba6108de0f
Haswanth Tamil <haswanthtamilofficial@gmail.com>
2025-06-17 22:32:46 +0530

    Nuked backend

--- backend/core/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Hotel
-
-class HotelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Hotel
-        fields = '__all__'  # includes all fields like is_liked, is_booked, etc.
--- backend/core/urls.py
+++
@@ -1,8 +0,0 @@
-# core/urls.py
-from django.urls import path
-from .views import get_hotels_raw, hotel_detail
-
-urlpatterns = [
-    path("hotels/", get_hotels_raw, name="hotel_list"),
-    path("hotels/<int:hotel_id>/", hotel_detail, name="hotel_detail"),
-]
