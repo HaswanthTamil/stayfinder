a0e6259a1e71d79d807388b4d68b44906ecdb075
Haswanth Tamil <haswanthtamilofficial@gmail.com>
2025-06-17 18:13:30 +0530

    Optimize production build.

--- frontend/src/app/bookings/[id]/page.tsx
+++ frontend/src/app/bookings/[id]/page.tsx
@@ -1,12 +1,16 @@
 // /bookings/[id]/page.tsx
+"use client"
 
 import BookingForm from "@/components/reusable/BookingForm"
+import { useParams } from "next/navigation"
 
-const NewBooking = ({ params }: { params: { id: number } }) => {
+const NewBooking = () => {
+  const { id } = useParams()
+  const id_num = typeof id === "string" ? parseInt(id, 10) : NaN
   return (
     <>
       <div className="flex flex-col items-center min-h-screen bg-gray-100 py-20 sm:px-20 gap-y-4 w-full">
-        <BookingForm id={params.id} />
+        <BookingForm id={id_num} />
       </div>
     </>
   )
--- frontend/src/app/explore/[id]/page.tsx
+++ frontend/src/app/explore/[id]/page.tsx
@@ -1,15 +1,19 @@
 // /explore/[id]/page.tsx
+"use client"
 
 import Details from "@/components/reusable/Details"
 import DetailSlider from "@/components/reusable/DetailSlider"
+import { useParams } from "next/navigation"
 
-const ExplorePage = ({ params }: { params: { id: number } }) => {
+const ExplorePage = () => {
+  const { id } = useParams()
+  const id_num = typeof id === "string" ? parseInt(id, 10) : NaN
   return (
     <>
       <div className="bg-gray-100 py-20 px-2 sm:px-20 items-center flex flex-col w-full min-h-screen">
         <div className="bg-gray-50 w-full items-center flex flex-col rounded-lg shadow-md p-4">
-          <DetailSlider hotel_id={params.id} />
-          <Details id={params.id} />
+          <DetailSlider hotel_id={id_num} />
+          <Details id={id_num} />
         </div>
       </div>
     </>
